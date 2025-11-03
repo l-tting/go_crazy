462becfcb5d383ba7a87a0266542ff4d2ab71522
l-tting <brianletting01@gmail.com>
2025-11-03 09:12:20 +0300

    pointer v cp receivers--structs

--- chapter1-core/9_structs.go
+++ chapter1-core/9_structs.go
@@ -2,18 +2,46 @@ package main
 
 import "fmt"
 
-func demoStruct(){
-	type Person struct{
-		name string
-		age int
-		address string
-		citizen bool
-	}
-	//struct variable - p is of type Person
-	var p Person
-	p.name = "Brian"
-	p.age = 26
-	p.address = "123 Kimathi St"
+type Person struct {
+	name    string
+	age     int
+	address string
+	citizen bool
+}
 
-	fmt.Println(p)
+// struct methods with receiver
+func (p Person) Greet() {
+	fmt.Println("Hello, my name is", p.name)
+}
+
+// method -> copy receiver -> param
+func (p Person) cook(meal string) {
+	fmt.Println(p.name, "can cook ", meal)
+
+}
+
+//pointer receiver
+func (p *Person) ChangeName(){
+	p.name = "Ethan"
+}
+
+func demoStruct() {
+
+	//struct variable - p is of type Person
+	var x Person
+	x.name = "Brian"
+	// p.age = 26
+	// p.address = "123 Kimathi St"
+
+	// short initialization
+	s := Person{name: "Brian", age: 21, address: "Nairobi", citizen: true}
+
+	// fmt.Println(p)
+	fmt.Println(s)
+
+	x.ChangeName()
+	x.Greet()
+	x.cook("pasta")
+
+
 }
